0c77685706cc9a62de1569f5dd3e1731fb4a4184
杨伟杰 <yangweijie@rongshikeji.cn>
2020-05-28 10:58:28 +0800

    init

--- app/src/main/java/com/app/androidwool/MainActivity.java
+++ app/src/main/java/com/app/androidwool/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.androidwool;
+package com.app.androidwool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.androidwool.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/app/androidwool/activity/AgentWebKitActivity.java
@@ -0,0 +1,19 @@
+package com.app.androidwool.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidwool.R;
+
+public class AgentWebKitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent_web_kit);
+    }
+
+
+
+}
